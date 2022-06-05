092ef9d25c48a7d19e590dbb23bbc715b82668c1
chimebukanian <chimebukaanyanwu@gmail.com>
2022-06-04 21:51:55 -0700

    update]

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-import string
-print(string.ascii_uppercase)
+if __name__ == "__main__":
+    import sys
+    no = len(sys.argv)
+    if no == 1:
+        print("{} arguments.".format(no - 1))
+    elif no == 2:
+        print("{} argument:".format(no - 1))
+    else:
+        print("{} arguments:".format(no - 1))
+
+    for i in range(1, no):
+        print("{}: {}".format(i, sys.argv[i]))
