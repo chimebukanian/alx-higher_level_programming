31370c077871fdd965ccd7b07f022eb62155e0f7
chimebukanian <chimebukaanyanwu@gmail.com>
2022-06-21 08:45:12 -0700

    update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def safe_print_integer(val):
+def safe_print_integer(value):
+
     try:
-        print("{:d}".format(val))
-        return True
-    except:
+        print('{:d}'.format(value))
+    except Exception as error:
         return False
+    else:
+        return True
--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,11 +3,11 @@ def magic_calculation(a, b):
     res = 0
     for i in range(1, 3):
         try:
-            if i > a:
+            if (i > a):
                 raise Exception('Too far')
             else:
-                res += a ** b / i
+                res += (a ** b) / i
         except:
             res = b + a
             break
-    return result
+    return (res)
