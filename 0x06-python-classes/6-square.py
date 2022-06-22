0ff31575a97f30b44d2a387ef8ba2b4b4db6119f
chimebukanian <chimebukaanyanwu@gmail.com>
2022-06-22 07:55:19 -0700

    done-fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,53 +1,60 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class
+Its implements value and type checks for its attributes
+Attributes:
+    area
+    my_print
+"""
+
+
 class Square:
-    """Defines a square"""
+    """Square implementation
+    """
     def __init__(self, size=0, position=(0, 0)):
-        """Initialises the data"""
         self.size = size
         self.position = position
 
-    def area(self):
-        """Returns current square area"""
-        return self.__size**2
-
     @property
     def size(self):
-        """Getter method"""
         return self.__size
 
     @size.setter
-    def size(self, value):
-        """Setter method"""
-        self.__size = value
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """calculates the square area
+        """
+        return (self.size ** 2)
 
     def my_print(self):
-        """Prints the square"""
-        if self.__size == 0:
-            print()
+        """prints a square  with the corresponding size
+        """
+        if (self.__size == 0):
+            print('')
         else:
-            for y in range(self.__position[1]):
-                print()
-            for i in range(self.__size):
-                for x in range(self.__position[0]):
-                    print(' ', end='')
-                for j in range(self.__size):
-                    print('#', end='')
-                print()
+            for i in range(self.position[1]):
+                print('')
+
+            for i in range(self.size):
+                print(' ' * self.position[0] + '#' * self.size)
 
     @property
     def position(self):
-        """Getter method"""
         return self.__position
 
     @position.setter
-    def position(self, value):
-        """Setter method"""
-        if type(value) != tuple or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if any(type(i) != int for i in value) or any(j < 0 for j in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+    def position(self, position):
+        if type(position) != tuple or \
+            len(position) != 2 or \
+            not all(isinstance(el, int) for el in position) or \
+                not all(el >= 0 for el in position):
+
+            raise TypeError('position must be a tuple of 2 positive integers')
+
+        self.__position = position
