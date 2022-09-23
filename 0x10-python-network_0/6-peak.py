33ac7d3d8ffa4acc77afcff8d4f4f6dbcd2fb7e6
chimebukanian <chimebukaanyanwu@gmail.com>
2022-09-22 21:52:31 -0700

    6peakfix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
+"""Defines a peak-finding algorithm"""
 
 
-def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
-
-    if list_of_integers is None or list_of_integers == []:
+def find_peak(ints_list):
+    """Returns a peak in a list of unsorted integers"""
+    if not ints_list:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+
+    length = len(ints_list)
+    if length == 1 or length == 2:
+        return max(ints_list)
+
+    midpoint = int(length / 2)
+    peak = ints_list[midpoint]
+    if ints_list[midpoint - 1] < peak > ints_list[midpoint + 1]:
+        return peak
+    elif peak < ints_list[midpoint - 1]:
+        return find_peak(ints_list[:midpoint])
+    else:
+        return find_peak(ints_list[midpoint + 1:])
